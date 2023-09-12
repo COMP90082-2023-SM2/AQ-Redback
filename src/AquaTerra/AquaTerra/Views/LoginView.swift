2fb04c5f4beb3bfc8bfe0930b0d8f33ffda99cd4
Yunqing Yu <88307851+YunqingYu@users.noreply.github.com>
2023-09-12 19:31:02 +1000

    finish implementing login front end

--- src/AquaTerra/AquaTerra/Views/LoginView.swift
+++ src/AquaTerra/AquaTerra/Views/LoginView.swift
@@ -14,51 +14,164 @@ struct LoginView: View {
     @State var username = ""
     @State var password = ""
     
+//    init(){
+//        for familyName in UIFont.familyNames {
+//            print(familyName)
+//            for fontName in UIFont.fontNames(forFamilyName: familyName){
+//                print("-- \(fontName)")
+//            }
+//        }
+//    }
+    
     var body: some View {
+        
         NavigationView {
             VStack {
-                // Header
-                VStack {
-                    Text("AquaTerra")
-                        .font(.system(size: 50))
-                        .bold()
+                ZStack {
+                    Image("Login")
+                        .frame(width: 600, height: 200)
+                        .offset(y:-250)
+                        .mask(
+                        Ellipse()
+                            .foregroundColor(Color.green)
+                            .frame(width: 600, height: 400)
+                            .offset(y:-250)
+                        )
                     
-                    // Login Form
-                    Form {
-                        TextField("Email Address", text: $username)
-                            .textFieldStyle(DefaultTextFieldStyle())
-                        SecureField("Password", text: $password)
-                            .textFieldStyle(DefaultTextFieldStyle())
-                        
-                        Button {
-                            sessionViewViewModel.login(
-                                username: username,
-                                password: password
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 76, height: 76)
+                        .offset(y:-50)
+                    
+                    
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 76, height: 76)
+                        .clipShape(Circle())
+                        .offset(y:-50)
+                        .overlay(Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                            .shadow(color: Color.black.opacity(0.10), radius: 3.0, x: 0, y: 4.0)
+                            .frame(width: 76, height: 76)
+                            .offset(y:-50)
                             )
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(Color.blue)
+                    
+
+                    
+                    
+               
+                    Text("AquaTerra")
+                        .font(.custom("OpenSans-ExtraBold", size: 20))
+                        .padding(.top, 20)
+                    
+                }.frame(width: 300, height: 100)
+                    .ignoresSafeArea()
+           
+                // Login Form
+                VStack{
+                    VStack(alignment: .center){
+                        TextField("Email", text: $username)
+                            .font(.custom("OpenSans-Regular", size: 14))
+                            .foregroundColor(Color("Placeholder"))
+                            .padding([.horizontal], 15)
+                            .padding(.vertical,20)
+                            .frame(height: 50)
+                            .frame(width: 318)
+                            .accentColor(Color("ButtonGradient2"))
+                        
+                    }
+                    .background(Color("Hint"))
+                    .cornerRadius(5)
+                    .padding(5)
+
+                    
+                    VStack(alignment: .center){
+                        SecureField("Password", text: $password)
+                            .font(.custom("OpenSans-Regular", size: 14))
+                            .foregroundColor(Color("Placeholder"))
+                            .padding([.horizontal], 15)
+                            .padding(.vertical,20)
+                            .frame(height: 50)
+                            .frame(width: 318)
+                            .accentColor(Color("ButtonGradient2"))
+                    }
+                    .background(Color("Hint"))
+                    .cornerRadius(5)
+                    .padding(5)
+                    
+                    VStack{
+                        Text("Forgot Password")
+                            .font(.custom("OpenSans-Regular", size: 14))
+                            .foregroundColor(Color("ButtonGradient2"))
+                            .frame(maxWidth: 318, alignment: .trailing)
+                    }
+                   
+                    
+                    VStack{
+                        Button {
+                        sessionViewViewModel.login(
+                            username: username,
+                            password: password
+                        )
+                    }
+                    
+                    label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                .fill(LinearGradient(gradient: Gradient(colors: [Color("ButtonGradient1"), Color("ButtonGradient2")]), startPoint: .leading, endPoint: .trailing))
+                                .frame(height: 50)
+                                .frame(width: 318)
                                 
-                                Text("Log in")
-                                    .foregroundColor(Color.white)
-                                    .bold()
+                            Text("Log in")
+                                .foregroundColor(Color.white)
+                                .font(.custom("OpenSans-Regular", size: 16))
+                                .bold()
+                                .padding([.horizontal], 15)
+                                .padding(.vertical,20)
+                                
                             }
                         }
-                        .padding(10)
                     }
+                
+                }
+                
+                ZStack{
+                    Divider().frame(width: 318)
+                    Text("or")
+                        .background(Color.white)
+                        .frame(width: 30)
+                        .font(.custom("OpenSans-Regular", size: 13))
+                        .foregroundColor(Color.gray)
                     
-                    // Create Account
-                    VStack {
-                        Text("New to here?")
-                        Button("Don't have an account? Sign Up.", action: sessionViewViewModel.showSignUp)
+                    
+                }
+                
+                
+                // Create Account
+                VStack {
+                    Button(action: sessionViewViewModel.showSignUp){
+                        ZStack {
+                        RoundedRectangle(cornerRadius: 5, style: .continuous)
+                            .fill(Color.black)
+                            .frame(height: 50)
+                            .frame(width: 318)
+                        
+                        Text("Create New Account")
+                            .foregroundColor(Color.white)
+                            .font(.custom("OpenSans-Regular", size: 16))
+                            .bold()
+                            .padding([.horizontal], 15)
+                            .padding(.vertical,20)
+                        
+                            }
                     }
-                    .padding(.bottom, 50)
                 }
             }
         }
     }
 }
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
