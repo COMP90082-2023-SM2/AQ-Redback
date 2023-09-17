a23726923ab6fa4a89006e4abd7c1ed1d3b5f50c
You Zhou <zjoyce668@gmail.com>
2023-09-17 20:49:16 +0800

    fix conflict in SessionView.swift

--- src/AquaTerra/AquaTerra/Views/SessionView.swift
+++ src/AquaTerra/AquaTerra/Views/SessionView.swift
@@ -15,9 +15,7 @@ struct SessionView: View {
     @State private var fieldData: [FieldData] = []
     @Binding var user: AuthUser
     @State private var isShowingSensorListView = false
-
     @State private var isShowingFarmsView = false
-    
     @ObservedObject private var viewModel = SessionViewViewModel()
     
     var body: some View {
@@ -30,86 +28,6 @@ struct SessionView: View {
                     
                     Spacer().frame(height: 60)
                     
-                    Text("Farm Management")
-                        .font(.custom("OpenSans-ExtraBold", size: 24))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.top, 76)
-                        .padding(.horizontal, 28)
-                        .padding(.bottom, 15)
-                }.ignoresSafeArea()
-                    .frame(maxHeight: 127)
-                
-                NavigationLink {
-                    MGatewaysView()
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.white)
-                            .frame(width: 318, height: 110)
-
-                        Image("Gateway")
-                            .resizable()
-                            .aspectRatio(2,contentMode: .fill)
-                            .frame(width: 318, height: 110)
-                            .cornerRadius(10)
-                            
-                    
-                        Text("My Gateways")
-                            .font(.custom("OpenSans-ExtraBold", size: 20))
-                            .foregroundColor(Color.white)
-                    }.frame(width: 318, height: 110)
-                       
-                }.padding(.top,13)
-
-                
-//                Button{
-//
-//                }label: {
-//
-//                }.padding(.top,13)
-                
-     
-//
-//                Text("You sign in as \(user.username) using Amplify!")
-//                    .font(.largeTitle)
-//                    .multilineTextAlignment(.center)
-                
-                Button {
-                    
-                    isShowingFarmsView.toggle()
-
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.white)
-                            .frame(width: 318, height: 110)
-
-                        Image("Farm")
-                            .resizable()
-                            .aspectRatio(2,contentMode: .fill)
-                            .frame(width: 318, height: 110)
-                            .cornerRadius(10)
-                            
-                        Text("My Farm and Fields")
-                            .font(.custom("OpenSans-ExtraBold", size: 20))
-                            .foregroundColor(Color.white)
-                    }.frame(width: 318, height: 110)
-                        
-                }
-                .navigationDestination(isPresented: $isShowingFarmsView) {
-                    
-                    FarmsView(viewModel: FarmsViewModel(currentUserName: user.username))
-                }
-
-
-                Button{
-                    sessionViewViewModel.fetchFieldData { result in
-                        switch result {
-                        case .success(let data):
-                            self.fieldData = data
-                            self.isShowingSensorListView = true
-                        case .failure(let error):
-                            print("Error fetching sensor data: \(error)")
                     VStack(spacing: 24){
                         NavigationLink {
                             MGatewaysView()
@@ -117,10 +35,15 @@ struct SessionView: View {
                             ManageCardView(imgName: "Gateway", TextName: "My Gateways")
                         }
                         
-                        Button{
-                            
-                        }label: {
+                        Button {
+                            isShowingFarmsView.toggle()
+                        } label: {
                             ManageCardView(imgName: "Farm", TextName: "My Farm and Fields")
+                                
+                        }
+                        .navigationDestination(isPresented: $isShowingFarmsView) {
+                            
+                            FarmsView(viewModel: FarmsViewModel(currentUserName: user.username))
                         }
                         
                         Button{
