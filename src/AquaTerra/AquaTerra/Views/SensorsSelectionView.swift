55245983dc84415f4c5c7ab73fd8460ba3e1f276
Bowei <b.huang@tektindustries.com>
2023-10-11 14:04:21 +1100

    Add Selection for register V1 or V2 sensors

---
+++ src/AquaTerra/AquaTerra/Views/SensorsSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SensorsSelectionView.swift
+//  AquaTerra
+//
+//  Created by Davincci on 11/10/2023.
+//
+
+import SwiftUI
+
+struct SensorsSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SensorsSelectionView()
+}
